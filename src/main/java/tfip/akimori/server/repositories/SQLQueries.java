6b2b3b7117b64656188a992af35da6c6597f9928
Akimori <wee.bscl@gmail.com>
2023-04-30 15:55:26 +0800

    placeholder needed sql queries

--- src/main/java/tfip/akimori/server/repositories/SQLQueries.java
+++ src/main/java/tfip/akimori/server/repositories/SQLQueries.java
@@ -3,10 +3,13 @@ package tfip.akimori.server.repositories;
 public interface SQLQueries {
 
     // CREATE
-    public final String SQL_INSERTUSER = """
+    public final String SQL_INSERT_USER = """
             INSERT INTO users(firstname, lastname, email, password, role) VALUES (?, ?, ?, ?, ?)
                 """;
-    public final String SQL_CREATENEWSTORE = """
+    public final String SQL_INSERT_STORE = """
+
+            """;
+    public final String SQL_INSERT_INSTRUMENT = """
 
             """;
 
@@ -14,15 +17,6 @@ public interface SQLQueries {
     public final String SQL_FINDBYEMAIL = """
             SELECT * FROM users WHERE email=?
                 """;
-    public final String SQL_GETINSTRUMENTSBYEMAIL = """
-            SELECT instrument_id, section, brand, model, serial_number, store_name, firstname, lastname
-            FROM instruments i
-            INNER JOIN store s
-            ON i.store_id = s.store_id
-            INNER JOIN users u
-            ON i.user_id = u.user_id
-            WHERE u.email=?;
-                    """;
     public final String SQL_GETSTOREBYEMAIL = """
             SELECT instrument_id, section, brand, model, serial_number, store_name, u2.firstname, u2.lastname, u2.email
             FROM instruments i
@@ -34,12 +28,27 @@ public interface SQLQueries {
             ON i.user_id = u2.user_id
             WHERE u.email=?;
                     """;
+    public final String SQL_GETINSTRUMENTSBYEMAIL = """
+            SELECT instrument_id, section, brand, model, serial_number, store_name, firstname, lastname, email
+            FROM instruments i
+            INNER JOIN store s
+            ON i.store_id = s.store_id
+            INNER JOIN users u
+            ON i.user_id = u.user_id
+            WHERE u.email=?;
+                    """;
 
     // UPDATE
-    public final String SQL_LOANOUT = """
+    public final String SQL_LOANOUT_INSTRUMENT = """
 
             """;
-    public final String SQL_RETURN = """
+    public final String SQL_RETURN_INSTRUMENT = """
+
+            """;
+    public final String SQL_ISSUE_CONSUMABLE = """
+
+            """;
+    public final String SQL_REPLENISH_CONSUMABLE = """
 
             """;
 }
